cfbdbf35ab8ce776ed3d2a9173f2c8a8e6883222
Rakesh-1221 <rakeshrajan711@gmail.com>
2024-12-10 16:22:27 +0530

    update form

--- Freshshop_app/forms.py
+++ Freshshop_app/forms.py
@@ -2,8 +2,20 @@ from django import forms
 from django.contrib.auth.models import User
 
 class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput, required=True)
-    
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+        required=True,
+        label="Password"
+    )
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+        }
+        labels = {
+            'username': 'Username',
+            'email': 'Email Address',
+        }
